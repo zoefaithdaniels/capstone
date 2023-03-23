c0a2221f5ebabe14158fc427aa217da9a2075a94
Zoe Daniels <zoefaithdaniels094>
2023-03-23 09:52:42 +0200

    idk anymore

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {errorHandling} = require('./middleware/errorHandling');
 const cookieParser = require('cookie-parser');
 
 app.use((req, res, next)=> {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Methods","*")
     res.header("Access-Control-Allow-Headers","*")
